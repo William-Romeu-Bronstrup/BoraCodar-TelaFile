8f67f2b378cc40beb12fa2f1f0c17af8f746e162
William Romeu <williambronstrup13@gmail.com>
2023-04-18 16:28:34 -0300

    Melhorando a UI e UX dos cards

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const inputFile = document.getElementById("inputFile")
 const cardNameOfFile = document.querySelector("article div > p")
 const cardSizeOfFile = document.querySelector("article div > span")
 
+const cardImg = document.getElementById("imgFile")
+const bgOfcardImg = document.querySelector(".bgLoading")
+const porcentageOfcardFile = document.getElementById("msgLoading")
+
 const downloadFile = document.getElementById("downloadFile")
 
 const progressBar = document.querySelector(".barLoading")
@@ -20,39 +24,43 @@ inputFile.addEventListener("change", (e) => {
     file.file = e.target.files[0]
 
     const leitor = new FileReader()
+    console.log(leitor.DONE)
 
     cardNameOfFile.textContent = file.name
     cardSizeOfFile.textContent = file.size
 
     readingFile(leitor)
   } else {
-    console.log("Arquivo não identificado")
+    addStylesWhenFileIsError()
   }
 })
 
 async function readingFile(leitor) {
   try {
+    await leitor.readAsText(file.file)
+    await leitor.addEventListener("load", () => {
+      let result = leitor.result
+      return creatingTagAToDownload(result, file.name)
+    })
+
     leitor.addEventListener("loadstart", () => {
-      console.log("Começou")
-      progressBar.value = 5
+      setStylesToDefault()
     })
 
     leitor.addEventListener("progress", () => {
       console.log("Em progresso")
+      //função para troca de img
 
       mainTag.style.cursor = "not-allowed"
       inputFile.style.display = "none"
     })
 
-    leitor.addEventListener("loadend", () => {
-      mainTag.style.cursor = "default"
-      inputFile.style.display = "block"
+    leitor.addEventListener("error", () => {
+      addStylesWhenFileIsError()
     })
 
-    await leitor.readAsText(file.file)
-    await leitor.addEventListener("load", () => {
-      let result = leitor.result
-      return creatingTagAToDownload(result, file.name)
+    leitor.addEventListener("loadend", () => {
+      addStylesWhenFileIsLoaded()
     })
   } catch (erro) {
     console.log(erro)
@@ -91,5 +99,44 @@ function transformSize(size) {
   }
 }
 
+function setStylesToDefault() {
+  progressBar.value = 0
+  cardImg.setAttribute("src", "assets/loadFile.svg")
+  cardImg.setAttribute("alt", "Imagem que representa um arquivo carregando")
+  bgOfcardImg.classList.remove("bgLoaded")
+  porcentageOfcardFile.setAttribute("id", "msgLoading")
+  porcentageOfcardFile.textContent = "0%"
+  progressBar.classList.remove("barLoaded")
+  progressBar.classList.add("barLoading")
+}
+
+function addStylesWhenFileIsError() {
+  console.log(file.name)
+  progressBar.value = 0
+  cardImg.setAttribute("src", "assets/errorFile.svg")
+  cardImg.setAttribute("alt", "Imagem que representa um arquivo não lido")
+  bgOfcardImg.classList.remove("bgLoaded")
+  bgOfcardImg.classList.add("bgError")
+  porcentageOfcardFile.setAttribute("id", "msgError")
+  porcentageOfcardFile.textContent = "Erro"
+  progressBar.classList.remove("barLoaded")
+  progressBar.classList.add("barError")
+}
+
+function addStylesWhenFileIsLoaded() {
+  mainTag.style.cursor = "default"
+  inputFile.style.display = "block"
+  progressBar.value = 100
+  cardImg.setAttribute("src", "assets/loadedFile.svg")
+  cardImg.setAttribute("alt", "Imagem que representa o arquivo carregado")
+  bgOfcardImg.classList.add("bgLoaded")
+  porcentageOfcardFile.setAttribute("id", "msgLoaded")
+  porcentageOfcardFile.textContent = "100%"
+  progressBar.classList.remove("barLoading")
+  progressBar.classList.add("barLoaded")
+}
+
 // Criar lógica para diminuir o tamanho do texto e motrar o início com o final.
 // Guardar os arquivos, um objeto ou array com o nome, size, url
+// Adicionar um X caso o usuário queira cancelar o download (funciont abort)
+// Aparecer a opção de download quando passar o mouse por cima do card, depois de concluído
