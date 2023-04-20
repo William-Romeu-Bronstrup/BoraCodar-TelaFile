4b96e0aed3375101282cdb42bc30f453edc810f9
William Romeu <williambronstrup13@gmail.com>
2023-04-20 02:03:53 -0300

    Arrumando

--- index.js
+++ index.js
@@ -1,101 +1,58 @@
 const mainTag = document.querySelector("main")
-const inputFile = document.getElementById("inputFile")
-// Mudar a forma de como buscar os elementos para não atrapalhar os que virão
-const cardNameOfFile = document.querySelector("article div > p")
-const cardSizeOfFile = document.querySelector("article div > span")
+const asideTag = document.querySelector("aside")
 
-const cardImg = document.getElementById("imgFile")
-const bgOfcardImg = document.querySelector(".bgLoading")
-const porcentageOfcardFile = document.getElementById("msgLoading")
+const inputFile = document.getElementById("inputFile")
+
 const downloadFile = document.getElementById("downloadFile")
-const progressBar = document.querySelector(".barLoading")
 
 let file = []
+
+while (downloadFile.hasChildNodes()) {
+  downloadFile.removeChild(downloadFile.firstChild)
+}
 
 !(function () {
   let getData = localStorage.getItem("file")
 
   if (getData) {
-    file.push(...JSON.parse(getData))
+    getLocalData(getData)
   }
 })()
 
-function buildCards() {}
+function getLocalData(getData) {
+  file.push(...JSON.parse(getData))
+  file.map((item) => {
+    asideTag.innerHTML += buildCards(item)
+  })
+}
 
 inputFile.addEventListener("change", (e) => {
-  while (downloadFile.hasChildNodes()) {
-    downloadFile.removeChild(downloadFile.firstChild)
-  }
-
   if (e.target.files[0] != undefined) {
     let obj = {
-      name: e.target.files[0].name,
-      size: transformSize(e.target.files[0].size),
-      file: e.target.files[0],
+      name: e.target.files.name,
+      size: transformSize(e.target.files.size),
+      file: e.target.files,
     }
 
-    cardNameOfFile.textContent = obj.name
-    cardSizeOfFile.textContent = obj.size
-
     file.push(obj)
+    console.log(obj)
 
     const leitor = new FileReader()
 
     readingFile(leitor, obj)
-
-    localStorage.setItem("file", JSON.stringify(file))
-  } else {
-    addStylesWhenFileIsError()
   }
 })
 
 async function readingFile(leitor, obj) {
   try {
     await leitor.readAsText(obj.file)
-    await leitor.addEventListener("load", () => {
-      return creatingTagAToDownload(leitor.result, obj.name)
-    })
-
-    leitor.addEventListener("loadstart", () => {
-      setStylesToDefault()
-    })
-
-    leitor.addEventListener("progress", () => {
-      console.log("Em progresso")
-      //função para troca de img
-
-      mainTag.style.cursor = "not-allowed"
-      inputFile.style.display = "none"
-    })
-
-    leitor.addEventListener("error", () => {
-      addStylesWhenFileIsError()
-    })
 
     leitor.addEventListener("loadend", () => {
-      addStylesWhenFileIsLoaded()
+      localStorage.setItem("file", JSON.stringify(file))
     })
   } catch (erro) {
     console.log(erro)
   }
-}
-
-function creatingTagAToDownload(result, fileName) {
-  const url = getFile(result)
-
-  const tagA = document.createElement("a")
-  tagA.textContent = "Download"
-
-  tagA.href = url
-  tagA.download = fileName
-  downloadFile.appendChild(tagA)
-}
-
-function getFile(result) {
-  const blob = new Blob([result], {
-    type: "octet/stream",
-  })
-  return URL.createObjectURL(blob)
 }
 
 function transformSize(size) {
@@ -112,46 +69,29 @@ function transformSize(size) {
   }
 }
 
-/*Estilos do card */
-function setStylesToDefault() {
-  progressBar.value = 0
-  cardImg.setAttribute("src", "assets/loadFile.svg")
-  cardImg.setAttribute("alt", "Imagem que representa um arquivo carregando")
-  bgOfcardImg.classList.remove("bgLoaded")
-  porcentageOfcardFile.setAttribute("id", "msgLoading")
-  porcentageOfcardFile.textContent = "0%"
-  progressBar.classList.remove("barLoaded")
-  progressBar.classList.add("barLoading")
-}
+function buildCards(item) {
+  return `
+    <article>
+      <div class="bgLoading bgLoaded">
+        <img
+          src="assets/loadedFile.svg"
+          alt="Imagem que representa um arquivo carregando"
+          id="imgFile"
+        />
+      </div>
+      <div>
+        <p>${item.name}</p>
+        <span>${item.size}</span>
 
-function addStylesWhenFileIsError() {
-  progressBar.value = 0
-  cardImg.setAttribute("src", "assets/errorFile.svg")
-  cardImg.setAttribute("alt", "Imagem que representa um arquivo não lido")
-  bgOfcardImg.classList.remove("bgLoaded")
-  bgOfcardImg.classList.add("bgError")
-  porcentageOfcardFile.setAttribute("id", "msgError")
-  porcentageOfcardFile.textContent = "Erro"
-  progressBar.classList.remove("barLoaded")
-  progressBar.classList.add("barError")
-}
-
-function addStylesWhenFileIsLoaded() {
-  mainTag.style.cursor = "default"
-  inputFile.style.display = "block"
-  progressBar.value = 100
-  cardImg.setAttribute("src", "assets/loadedFile.svg")
-  cardImg.setAttribute("alt", "Imagem que representa o arquivo carregado")
-  bgOfcardImg.classList.remove("bgError")
-  bgOfcardImg.classList.add("bgLoaded")
-  porcentageOfcardFile.setAttribute("id", "msgLoaded")
-  porcentageOfcardFile.textContent = "100%"
-  progressBar.classList.remove("barLoading")
-  progressBar.classList.remove("barError")
-  progressBar.classList.add("barLoaded")
+        <footer>
+          <progress class="barLoaded" max="100" value="100"></progress>
+          <span id="msgLoaded">100%</span>
+        </footer>
+      </div>
+    </article>
+  `
 }
 
 // Criar lógica para diminuir o tamanho do texto e motrar o início com o final.
-// Guardar os arquivos, um objeto ou array com o nome, size, url
 // Adicionar uma lixeira para excluir o file
 // Fazer o multiply funcionar
